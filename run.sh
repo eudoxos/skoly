2a4b3f944db12d271eef324f7ff33ea89bb2c658
eudoxos <eu@doxos.eu>
2021-03-08 08:53:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+# run this script with sudo
 snap install zoom-client teams
 apt update
 apt -y dist-upgrade
 apt install webext-ublock-origin
 # add zoom, teams, lowriter, loimpress, firefox launchers to the panel
-APPLETSRC=~/.config/plasma-org.kde.plasma.desktop-appletsrc
-cp $APPLETSRC $APPLETSRC~
-echo > $APPLETSRC <<END
-[Containments][0][Applets][xx][Configuration
-localPath=...
-url=...
-
-
-END
-cp $APPLETSRC /etc/skel/.config/
+# just put the config file from github to /etc/skel for new users
+APPLETS_CFG=plasma-org.kde.plasma.desktop-appletsrc
+wget https://github.com/eudoxos/skoly/blob/main/$APPLETS_CFG -O /etc/skel/.config/$APPLETS_CFG
+##
+apt install -y lm-sensors fancontrol
+sensors-detect # this is interactive
+/etc/init.d/kmod start
+pwmconfig
