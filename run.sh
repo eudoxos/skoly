8429140d4c5f06b7290f1d8ff352f68776f3498c
eudoxos <eu@doxos.eu>
2021-03-09 18:59:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,22 @@ snap install zoom-client teams
 apt update
 apt -y dist-upgrade
 apt install -y webext-ublock-origin vim
+## this is for KDE Plasma
 # add zoom, teams, lowriter, loimpress, firefox launchers to the panel
 # just put the config file from github to /etc/skel for new users
 APPLETS_CFG=plasma-org.kde.plasma.desktop-appletsrc
 wget https://github.com/eudoxos/skoly/raw/main/$APPLETS_CFG -O /etc/skel/.config/$APPLETS_CFG
-##
+## this is for gnome-shell
+# see https://askubuntu.com/a/1193560/24839
+# and https://help.gnome.org/admin/system-admin-guide/stable/dconf-custom-defaults.html.en
+# gsettings set org.gnome.shell favorite-apps ""
+echo -e "user-db:user\nsystem-db:local\n" >> /etc/dconf/profile/user
+mkdir -p /etc/dconf/db/local.d
+echo <<FAVORITES
+[org/gnome/shell]
+favorite-apps=['firefox.desktop','org.gnome.Nautilus.desktop','libreoffice-writer.desktop','libreoffice-impress.desktop','','','snap-store_ubuntu-software.desktop','yelp.desktop']
+FAVORITES
+dconfig update
 ## most laptops don't need this
 ##
 # apt install -y lm-sensors fancontrol
